68be4f1da4195552bf35d29431cefdb7fbc5baf7
Ahmad <ahmadalhatlan@gmail.com>
2025-10-01 20:41:56 +0300

    Add frontend app service terraform 2

--- infra/frontend_app.tf
+++ infra/frontend_app.tf
@@ -1,26 +1,32 @@
+
 resource "azurerm_service_plan" "frontend_plan" {
-  name                = "frontend-service-plan"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  name                = "frontend-service-plan-aalhatlan"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   os_type             = "Linux"
-  sku_name            = "B1" 
+  sku_name            = "B1"   
 }
 
 
 resource "azurerm_linux_web_app" "frontend_app" {
   name                = "frontend-app-aalhatlan"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   service_plan_id     = azurerm_service_plan.frontend_plan.id
 
   site_config {
     application_stack {
-      docker_image     = "aalhatlan/assignment-fe"
-      docker_image_tag = "latest"
+      docker_image     = "aalhatlan/assignment-fe"  
+      docker_image_tag = "latest"                   
     }
   }
 
   app_settings = {
-    WEBSITES_PORT = "80" 
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    DOCKER_REGISTRY_SERVER_URL          = "https://index.docker.io"  
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
