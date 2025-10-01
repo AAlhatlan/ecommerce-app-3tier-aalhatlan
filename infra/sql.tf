5e5d5fa86cb6deab118dcd599a2c67a9ec853bb6
Ahmad <ahmadalhatlan@gmail.com>
2025-10-01 22:23:02 +0300

    Add frontend app service terraform sql

---
+++ infra/sql.tf
@@ -0,0 +1,23 @@
+resource "azurerm_mssql_server" "sql_server" {
+  name                          = "ecommerce-sqlserver-aalhatlan"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  version                       = "12.0"
+  administrator_login           = var.sql_admin_login
+  administrator_login_password  = var.sql_admin_password
+  public_network_access_enabled = true
+}
+
+resource "azurerm_mssql_database" "sql_database" {
+  name      = "ecommerce_db"
+  server_id = azurerm_mssql_server.sql_server.id
+  sku_name  = "Basic"
+}
+
+
+resource "azurerm_mssql_firewall_rule" "github_actions_access" {
+  name             = "github-actions-access"
+  server_id        = azurerm_mssql_server.sql_server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -43,3 +43,29 @@ variable "be_tag" {
   description = "Docker image tag (e.g. latest)"
 }
 
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the existing Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region where resources will be created"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "Admin username for SQL Server"
+  sensitive   = true
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "Admin password for SQL Server"
+  sensitive   = true
+}
