b361f7872d34f16769d2c0c159e6edb0c721bb5b
Ahmad <ahmadalhatlan@gmail.com>
2025-10-01 18:14:41 +0300

    test 3

--- infra/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "resource_group" { value = azurerm_resource_group.rg.name }
-output "frontend_fqdn" { value = module.aca.frontend_fqdn }
-output "backend_fqdn" { value = module.aca.backend_fqdn }
-output "sql_fqdn" { value = module.sql.sql_server_fqdn }
-output "sql_db" { value = module.sql.database_name }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,48 +1,14 @@
-variable "project_name" {
-  description = "اسم المشروع"
-  type        = string
-}
-
 variable "resource_group_name" {
-  description = "اسم مجموعة الموارد"
   type        = string
+  description = "Name of the Resource Group"
 }
 
 variable "location" {
-  description = "موقع النشر"
   type        = string
-  default     = "westeurope"
+  description = "Azure region where resources will be created"
 }
 
-variable "backend_image" {
-  description = "رابط صورة الباكند على DockerHub"
-  type        = string
+variable "tags" {
+  type        = map(string)
+  description = "Tags to assign to resources"
 }
-
-variable "frontend_image" {
-  description = "رابط صورة الفرونت على DockerHub"
-  type        = string
-}
-
-variable "sql_admin_login" {
-  description = "اسم مستخدم SQL Server"
-  type        = string
-}
-
-variable "sql_admin_password" {
-  description = "كلمة مرور SQL Server"
-  type        = string
-  sensitive   = true
-}
-
-variable "aca_subnet_id" {
-  description = "Subnet ID for ACA environment"
-  type        = string
-}
-
-variable "sql_fqdn" {
-  description = "Fully qualified domain name of the SQL server"
-  type        = string
-}
-
-resource_group_name
