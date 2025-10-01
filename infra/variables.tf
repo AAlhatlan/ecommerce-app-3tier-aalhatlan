427d856393599637db192a933e1e2f404f6fb680
Ahmad <ahmadalhatlan@gmail.com>
2025-10-01 22:25:42 +0300

    Add frontend app service terraform sql  1

--- infra/variables.tf
+++ infra/variables.tf
@@ -43,21 +43,6 @@ variable "be_tag" {
   description = "Docker image tag (e.g. latest)"
 }
 
-variable "subscription_id" {
-  type        = string
-  description = "Azure Subscription ID"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the existing Resource Group"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region where resources will be created"
-}
-
 variable "sql_admin_login" {
   type        = string
   description = "Admin username for SQL Server"
