ec26f04dfe88e37b19e2860069f9a11d00ba8882
AAlhatlan <ahmadalhatlan@gmail.com>
2025-10-01 11:39:18 +0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,49 +1,32 @@
 terraform {
-  required_version = ">= 1.5.0"
-
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
 
+# 🚫 تعطيل الموديولات مؤقتًا
+# module "aca" {
+#   source = "./modules/aca"
+#   # arguments...
+# }
 
+# module "sql" {
+#   source = "./modules/sql"
+#   # arguments...
+# }
 
-# 🟦 إنشاء Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "temporary-rg"
+  location = "westeurope"
 }
 
-# 🌐 استدعاء Module الشبكة
-module "network" {
-  source = "./modules/network"
-
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
+output "rg_name" {
+  value = azurerm_resource_group.rg.name
 }
-
-# 🧰 استدعاء Module ACA (Backend + Frontend)
-module "aca" {
-  source = "./modules/aca"
-
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  backend_image       = var.backend_image
-  frontend_image      = var.frontend_image
-  project_name        = var.project_name
-}
-
-# 🗄️ استدعاء Module SQL
-module "sql" {
-  source = "./modules/sql"
-
-  project_name         = var.project_name
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  sql_admin_login      = var.sql_admin_login
-  sql_admin_password   = var.sql_admin_password
-  sql_pe_subnet_id     = var.sql_pe_subnet_id
-  private_dns_zone_name = var.private_dns_zone_name
-}
-
-
-
-
