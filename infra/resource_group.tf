8c1f83e3c1f7d0d5240594201585bf25402d9c63
Ahmad <ahmadalhatlan@gmail.com>
2025-10-05 14:07:43 +0300

    var3

--- infra/resource_group.tf
+++ infra/resource_group.tf
@@ -1,5 +1,7 @@
 resource "azurerm_resource_group" "resource_group" {
-  name     = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
+name     = var.resource_group_name
+ location = var.location
+
+ tags = var.tags
 }
 
